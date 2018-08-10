b7d0da02827bd75620c6d8cc939f4dedeb40d186
kazinur <kazinur@mybihs.org>
2018-08-10 03:43:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
-# dog.rb
+class Dog
+  def bark  
+    puts "Woof!"
+end 
+Dog.new.bark 
+
+def sit 
+  puts "The dog is sitting"
+end 
+end 
+Dog.new.sit 
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
-# person.rb
+class Person 
+  def talk 
+    puts "Hello world!"
+  end 
+  Person.new.talk 
+  
+  def walk 
+    puts "The person is walking"
+  end 
+end 
+Person.new.walk  
